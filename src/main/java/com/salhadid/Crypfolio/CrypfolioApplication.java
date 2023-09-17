fae67ac103ec290ce2f6c0988bc7d4c782d33db3
smalhadid <smalhadid@gmail.com>
2023-09-17 19:27:16 -0400

    initial commit

---
+++ src/main/java/com/salhadid/Crypfolio/CrypfolioApplication.java
@@ -0,0 +1,13 @@
+package com.salhadid.Crypfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrypfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrypfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salhadid/Crypfolio/CrypfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salhadid.Crypfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrypfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
